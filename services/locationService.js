a172f84f5ecd5aff884ad02f102702c837d93c16
mukeshlaxmi90 <mukeshlaxmi90@gmail.com>
2025-09-14 12:10:19 +0530

    14Sep

--- services/locationService.js
+++ services/locationService.js
@@ -1,10 +1,10 @@
 const db = require('../Conn_db/connection');
   console.log("L8");
-const saveLocation = (lat, lon, address,ip) => {
-  console.log('location called', lat, lon, address);
+const saveLocation = (lat, lon, address,localIP) => {
+  console.log('location called', lat, lon, address,localIP);
   return new Promise((resolve, reject) => {
     const sql = "INSERT INTO user_locations (latitude, longitude, address,ip_address) VALUES ($1, $2, $3,$4) RETURNING id";
-    db.query(sql, [lat, lon, address,ip], (err, result) => {
+    db.query(sql, [lat, lon, address,localIP], (err, result) => {
       if (err) return reject(err);
       resolve(result.rows[0].id); // Postgres में insertId  होता hai
     });
